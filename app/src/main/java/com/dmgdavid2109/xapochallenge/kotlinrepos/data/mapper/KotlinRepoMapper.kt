106749990361e0b9cc4832f3fdda101c315e0243
David Martinez Garcia <david.martinez@fundingcircle.com>
2020-08-05 16:52:38 +0200

    Creates KotlinRepoMapper

---
+++ app/src/main/java/com/dmgdavid2109/xapochallenge/kotlinrepos/data/mapper/KotlinRepoMapper.kt
@@ -0,0 +1,19 @@
+package com.dmgdavid2109.xapochallenge.kotlinrepos.data.mapper
+
+import com.dmgdavid2109.xapochallenge.common.data.Mapper
+import com.dmgdavid2109.xapochallenge.kotlinrepos.data.model.KotlinRepoDTO
+import com.dmgdavid2109.xapochallenge.kotlinrepos.domain.model.KotlinRepo
+import javax.inject.Inject
+
+class KotlinRepoMapper @Inject constructor() : Mapper<KotlinRepoDTO, KotlinRepo> {
+
+    override fun map(input: KotlinRepoDTO): KotlinRepo {
+        with(input) {
+            return KotlinRepo(
+                input.name,
+                input.description,
+                input.avatarUrl
+            )
+        }
+    }
+}
