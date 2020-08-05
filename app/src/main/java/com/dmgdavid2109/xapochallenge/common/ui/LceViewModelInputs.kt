ecaea257027281464c6beb35b36768af11581870
David Martinez Garcia <david.martinez@fundingcircle.com>
2020-08-05 17:36:59 +0200

    Creates LCE states for displaying the different states

---
+++ app/src/main/java/com/dmgdavid2109/xapochallenge/common/ui/LceViewModelInputs.kt
@@ -0,0 +1,5 @@
+package com.dmgdavid2109.xapochallenge.common.ui
+
+interface LceViewModelInputs {
+    fun retry()
+}
---
+++ app/src/main/java/com/dmgdavid2109/xapochallenge/common/ui/LceViewState.kt
@@ -0,0 +1,6 @@
+package com.dmgdavid2109.xapochallenge.common.ui
+
+interface LceViewState {
+    val isLoading: Boolean
+    val errorMessage: Int?
+}
