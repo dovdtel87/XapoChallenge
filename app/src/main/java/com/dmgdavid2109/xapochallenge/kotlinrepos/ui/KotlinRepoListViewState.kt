e632d38a418d7d401d41d0649b395a04c48c18b4
David Martinez Garcia <david.martinez@fundingcircle.com>
2020-08-05 17:37:28 +0200

    Creates KotlinRepoListViewState

---
+++ app/src/main/java/com/dmgdavid2109/xapochallenge/kotlinrepos/ui/KotlinRepoListViewState.kt
@@ -0,0 +1,10 @@
+package com.dmgdavid2109.xapochallenge.kotlinrepos.ui
+
+import com.dmgdavid2109.xapochallenge.common.ui.LceViewState
+import com.dmgdavid2109.xapochallenge.kotlinrepos.domain.model.KotlinRepo
+
+data class KotlinRepoListViewState(
+    override val isLoading: Boolean = true,
+    override val errorMessage: Int? = null,
+    val repositoryList: List<KotlinRepo> = emptyList()
+) : LceViewState
