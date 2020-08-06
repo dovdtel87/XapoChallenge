4a670ce7d4f8d20c2c11df16fed1dec560ed6cc0
David Martinez Garcia <david.martinez@fundingcircle.com>
2020-08-06 14:04:44 +0200

    Creates DetailsViewModel

--- app/src/main/java/com/dmgdavid2109/xapochallenge/kotlinrepos/ui/details/KotlinRepoDetailViewModel.kt
+++ app/src/main/java/com/dmgdavid2109/xapochallenge/kotlinrepos/ui/details/KotlinRepoDetailViewModel.kt
@@ -3,16 +3,29 @@ package com.dmgdavid2109.xapochallenge.kotlinrepos.ui.details
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.dmgdavid2109.xapochallenge.common.ui.ViewStateLiveData
+import com.dmgdavid2109.xapochallenge.di.ViewModelFactoryCreator
+import com.dmgdavid2109.xapochallenge.kotlinrepos.domain.model.KotlinRepo
 import javax.inject.Inject
 
-class KotlinRepoDetailViewModel @Inject constructor() : ViewModel(),
+class KotlinRepoDetailViewModel(
+    private val kotlinRepo: KotlinRepo
+) : ViewModel(),
     KotlinRepoDetailViewModelInput {
 
-    private val _viewState = ViewStateLiveData(KotlinRepoDetailViewState())
+    private val _viewState = ViewStateLiveData(KotlinRepoDetailViewState()).apply {
+        updateCurrentState {
+            copy(kotlinRepo = this@KotlinRepoDetailViewModel.kotlinRepo)
+        }
+    }
     val viewState: LiveData<KotlinRepoDetailViewState>
         get() = _viewState
 
     override fun retry() {
 
+    }
+
+    class Factory @Inject constructor() {
+        fun create(kotlinRepo: KotlinRepo) =
+            ViewModelFactoryCreator.createForViewModel { KotlinRepoDetailViewModel(kotlinRepo) }
     }
 }
--- app/src/main/java/com/dmgdavid2109/xapochallenge/kotlinrepos/ui/details/KotlinRepoDetailViewState.kt
+++ app/src/main/java/com/dmgdavid2109/xapochallenge/kotlinrepos/ui/details/KotlinRepoDetailViewState.kt
@@ -1,13 +1,10 @@
 package com.dmgdavid2109.xapochallenge.kotlinrepos.ui.details
 
 import com.dmgdavid2109.xapochallenge.common.ui.LceViewState
+import com.dmgdavid2109.xapochallenge.kotlinrepos.domain.model.KotlinRepo
 
 data class KotlinRepoDetailViewState(
     override val isLoading: Boolean = true,
     override val errorMessage: Int? = null,
-    val name: String = "",
-    val description: String = "",
-    val author: String = "",
-    val starts: String = "",
-    val forks: String = ""
+    val kotlinRepo: KotlinRepo = KotlinRepo("","","", "", "", "")
 ) : LceViewState
